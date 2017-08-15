00e5577de843a7fda524932dce5bf8e5e9d73e22
zshi <zshi@redhat.com>
2017-08-15 18:53:39 +0800

    upgrade-ansible: move ansible plays into playbooks folder

--- tools/run_upgrade_ansible.sh
+++ tools/run_upgrade_ansible.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ansible-playbook ../upgrade-ansible/upgrade.yaml -e compute_node='overcloud-compute-0'
-ansible-playbook ../upgrade-ansible/upgrade.yaml -e compute_node='overcloud-compute-2'
+ansible-playbook ../upgrade-ansible/playbooks/upgrade.yaml -e compute_node='overcloud-compute-0'
+ansible-playbook ../upgrade-ansible/playbooks/upgrade.yaml -e compute_node='overcloud-compute-2'
